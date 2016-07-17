0e479eed0a4b62926a28677995787725a46e8f53
Frank Ganske <frank.ganske@web.de>
2016-07-17 21:30:22 +0200

    Reorder initialization of ChannelPipeline
    A ChannelPipeline invokes the read (Inbound) handlers in ascending
    ordering and then the write (Outbound) handlers in descending ordering.
    
    Regarding the Javadoc of HttpObjectAggregator it's needed to have the
    HttpResponseEncoder or HttpRequestEncoder before the
    HttpObjectAggregator in the ChannelPipeline.

--- src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
+++ src/main/java/org/littleshoot/proxy/impl/ProxyToServerConnection.java
@@ -876,7 +876,16 @@ public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
     }
 
     /**
-     * Initialize our {@link ChannelPipeline}.
+     * Initialize our {@link ChannelPipeline} to connect the upstream server.
+     * LittleProxy acts as a client here.
+     * 
+     * A {@link ChannelPipeline} invokes the read (Inbound) handlers in
+     * ascending ordering of the list and then the write (Outbound) handlers in
+     * descending ordering.
+     * 
+     * Regarding the Javadoc of {@link HttpObjectAggregator} it's needed to have
+     * the {@link HttpResponseEncoder} or {@link HttpRequestEncoder} before the
+     * {@link HttpObjectAggregator} in the {@link ChannelPipeline}.
      * 
      * @param pipeline
      * @param httpRequest
@@ -889,11 +898,13 @@ public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
         }
 
         pipeline.addLast("bytesReadMonitor", bytesReadMonitor);
+        pipeline.addLast("bytesWrittenMonitor", bytesWrittenMonitor);
+
+        pipeline.addLast("encoder", new HttpRequestEncoder());
         pipeline.addLast("decoder", new HeadAwareHttpResponseDecoder(
                 8192,
                 8192 * 2,
                 8192 * 2));
-        pipeline.addLast("responseReadMonitor", responseReadMonitor);
 
         // Enable aggregation for filtering if necessary
         int numberOfBytesToBuffer = proxyServer.getFiltersSource()
@@ -902,8 +913,7 @@ public class ProxyToServerConnection extends ProxyConnection<HttpResponse> {
             aggregateContentForFiltering(pipeline, numberOfBytesToBuffer);
         }
 
-        pipeline.addLast("bytesWrittenMonitor", bytesWrittenMonitor);
-        pipeline.addLast("encoder", new HttpRequestEncoder());
+        pipeline.addLast("responseReadMonitor", responseReadMonitor);
         pipeline.addLast("requestWrittenMonitor", requestWrittenMonitor);
 
         // Set idle timeout
